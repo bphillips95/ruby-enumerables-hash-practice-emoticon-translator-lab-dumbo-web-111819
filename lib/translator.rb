7f8095222e1602c3406626f429a290dc0a86106a
Baruch Phillips <baruchphillips@gmail.com>
2019-11-13 22:57:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,14 @@ def load_library(library)
  new_hash = { 
    "get meaning": {},
    'get emoticon': {} }
-hash
+
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
+  end
+
+  new_hash
+end
 end
 
 
